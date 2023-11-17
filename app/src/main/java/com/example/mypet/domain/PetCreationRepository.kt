3cd963eca64063d110e703598f3cdf3810b8e39b
akimychau <akimychau@gmail.com>
2023-11-17 15:06:26 +0100

    Добавление питомца в ДБ(без проверки)

--- app/src/main/java/com/example/mypet/domain/PetCreationRepository.kt
+++ app/src/main/java/com/example/mypet/domain/PetCreationRepository.kt
@@ -1,12 +1,18 @@
 package com.example.mypet.domain
 
+import com.example.mypet.data.local.room.entity.LocalPetMyEntity
 import com.example.mypet.data.local.room.model.LocalPetBreedModel
 import com.example.mypet.data.local.room.model.LocalPetKindModel
+import com.example.mypet.data.local.room.model.LocalPetModel
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.flow.Flow
+import java.util.Date
 
 interface PetCreationRepository {
 
     fun getKindList(): Flow<List<LocalPetKindModel>>
 
     fun getBreedList(kindId: Int): Flow<List<LocalPetBreedModel>>
+
+    suspend fun addNewPetToDb(name: String, kindId: Int, breedId: Int, dateOfBirth: String, weight: Int)
 }
