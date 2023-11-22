02af8911004e9e1780fc1f8f7b144000c5199b03
Fix <zfix.amursk@ya.ru>
2023-11-22 20:13:21 +1000

    Добавлен фаб. Добалено скрытие и открытие в зависимости от текущего фрагмента

--- app/src/main/java/com/example/mypet/ui/MainActivity.kt
+++ app/src/main/java/com/example/mypet/ui/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.mypet.ui
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,10 +17,11 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.mypet.app.R
 import com.example.mypet.app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
     private val binding by viewBinding(ActivityMainBinding::bind)
 
     private val topLevelDestinations = setOf(
@@ -38,10 +39,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         navHost.navController
     }
 
+    private val fabDestinations = setOf(
+        R.id.foodDetailFragment,
+        R.id.careDetailFragment,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestPermissionForOverlay()
+//        requestPermissionForOverlay()
 
         setSupportActionBar(binding.toolbar)
 
@@ -49,12 +55,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             this@MainActivity, navController, mAppBarConfiguration
         )
 
-
         binding.bottomNavigation.setupWithNavController(navController)
+        observeNavigation()
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
-            != PackageManager.PERMISSION_GRANTED
-        ) requestPermissionForOverlay()
+        /*        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED
+                ) requestPermissionForOverlay()*/
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -66,6 +72,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         if (!Settings.canDrawOverlays(this)) {
             val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
             startActivityForResult(intent, 0)
+        }
+    }
+
+    private fun observeNavigation() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                navController.currentBackStack.collectLatest {
+                    if (fabDestinations.contains(it.last().destination.id))
+                        binding.floatingActionButton.show()
+                    else
+                        binding.floatingActionButton.hide()
+                }
+            }
         }
     }
 }
