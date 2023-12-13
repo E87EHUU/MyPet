8044043e7ed4e978f06147dbc2e3fcb82bab4615
Fix <zfix.amursk@ya.ru>
2023-12-13 20:38:37 +1000

    UPDATE добавлены типы ухода

--- app/src/main/java/com/example/mypet/domain/care/CareTypes.kt
+++ app/src/main/java/com/example/mypet/domain/care/CareTypes.kt
@@ -4,7 +4,15 @@ import com.example.mypet.app.R
 
 enum class CareTypes(val titleResId: Int, val iconResId: Int) {
     FOOD(R.string.care_food_title, R.drawable.feed),
+    WALK(R.string.care_walk_title, R.drawable.home),
     BATH(R.string.care_bath_title, R.drawable.ic_care_bath),
+    MEDICINE(R.string.care_medicine_title, R.drawable.home),
+    GROOMING(R.string.care_grooming_title, R.drawable.home),
+    TRAINING(R.string.care_training_title, R.drawable.home),
+    VACCINATION(R.string.care_vaccination_title, R.drawable.home),
+    VITAMIN(R.string.care_vitamin_title, R.drawable.home),
+    CHECKUP(R.string.care_checkup_title, R.drawable.home),
+    TOOTHBRUSHING(R.string.care_toothbrushing_title, R.drawable.home),
     COMBING_THE_WOOL(R.string.care_combing_the_wool_title, R.drawable.ic_care_brushing),
     AGAINST_FLEAS_WORMS(
         R.string.care_against_worms_title,
@@ -14,5 +22,4 @@ enum class CareTypes(val titleResId: Int, val iconResId: Int) {
         R.string.care_against_fleas_and_ticks_title,
         R.drawable.ic_care_against_fleas_and_ticks
     ),
-    WALK(R.string.care_walk_title, R.drawable.home)
 }
