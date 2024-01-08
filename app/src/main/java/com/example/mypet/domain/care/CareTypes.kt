34cc8c326057602d45b580a6ddc4da15e70c3240
Fix <zfix.amursk@ya.ru>
2024-01-08 09:05:01 +0300

    FIX иконки ухода

--- app/src/main/java/com/example/mypet/domain/care/CareTypes.kt
+++ app/src/main/java/com/example/mypet/domain/care/CareTypes.kt
@@ -4,16 +4,16 @@ import com.example.mypet.app.R
 
 enum class CareTypes(val titleResId: Int, val iconResId: Int) {
     FOOD(R.string.care_food_title, R.drawable.feed),
-    WALK(R.string.care_walk_title, R.drawable.home),
+    WALK(R.string.care_walk_title, R.drawable.ic_care_walking),
+
     BATH(R.string.care_bath_title, R.drawable.ic_care_bath),
-    MEDICINE(R.string.care_medicine_title, R.drawable.home),
-    GROOMING(R.string.care_grooming_title, R.drawable.home),
-    TRAINING(R.string.care_training_title, R.drawable.home),
-    VACCINATION(R.string.care_vaccination_title, R.drawable.home),
-    VITAMIN(R.string.care_vitamin_title, R.drawable.home),
-    CHECKUP(R.string.care_checkup_title, R.drawable.home),
-    TOOTHBRUSHING(R.string.care_toothbrushing_title, R.drawable.home),
+    TOOTHBRUSHING(R.string.care_toothbrushing_title, R.drawable.ic_care_bath),
+
     COMBING_THE_WOOL(R.string.care_combing_the_wool_title, R.drawable.ic_care_brushing),
+
+    MEDICINE(R.string.care_medicine_title, R.drawable.ic_care_vaccine),
+    VACCINATION(R.string.care_vaccination_title, R.drawable.ic_care_vaccine),
+    VITAMIN(R.string.care_vitamin_title, R.drawable.ic_care_vits),
     AGAINST_FLEAS_WORMS(
         R.string.care_against_worms_title,
         R.drawable.ic_care_against_worms
@@ -22,4 +22,8 @@ enum class CareTypes(val titleResId: Int, val iconResId: Int) {
         R.string.care_against_fleas_and_ticks_title,
         R.drawable.ic_care_against_fleas_and_ticks
     ),
+
+    GROOMING(R.string.care_grooming_title, R.drawable.ic_care_brushing),
+    TRAINING(R.string.care_training_title, R.drawable.ic_care_training),
+    CHECKUP(R.string.care_checkup_title, R.drawable.home),
 }
